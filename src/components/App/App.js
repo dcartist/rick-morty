cd3602ba30bf75f4d34b94a45d657e41c1ee4a52
DCartist <pbannerman13@gmail.com>
2019-08-22 15:30:46 -0400

    adding intro

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Home from '../Home/Home.js'
-import logo from '../Images/portal.gif';
-import name from '../Images/rickmorty.png'
+import Intro from '../Intro/Intro.js'
+
 import './App.css';
 import {Route, Link, Switch, Redirect} from "react-router-dom";
 
@@ -25,22 +25,7 @@ class App extends Component {
   render(){
     return (
       <div className="App">
-      <header className="App-header">
-        <img src={name} className="name" alt="rick and morty"/>
-        <p className="title"> Battle Royalle</p>
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+        <Intro></Intro>
       <Route path="/" exact component={Home}/>
       {/* <Route path="/show/:name" render={(props)=> <Show setInfo={this.setInfo} {...props} {...this.state} />} /> */}
 
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import logo from '../Images/portal.gif';
+import name from '../Images/rickmorty.png'
+import '../App/App.css';
 
 class Intro extends Component {
     render() {
         return (
             <div>
-                
+                      <header className="App-header">
+        <img src={name} className="name" alt="rick and morty"/>
+        <p className="title"> Battle Royalle</p>
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        {/* <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
             </div>
         );
     }
